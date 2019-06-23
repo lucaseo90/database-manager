da54773deee6ff3d1e5e0da2d3b41de4a6fc91bf
Hyunseok Seo <hsseo0501@gmail.com>
2019-06-23 14:07:02 +0900

    Init commit

---
+++ src/main/java/io/github/hsseo0501/databasemanager/DatabaseManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.hsseo0501.databasemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/hsseo0501/databasemanager/DatabaseManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.hsseo0501.databasemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseManagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
