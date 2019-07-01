d96b5a8fd84ff7dbb39eacb9d23a33df7249126f
Hyunseok Seo <hsseo0501@gmail.com>
2019-07-01 13:46:38 +0900

    [JDBC-METADATA-CH08-C1] Added HelloServlet

--- src/main/java/io/github/hsseo0501/databasemanager/DatabaseManagerApplication.java
+++ src/main/java/io/github/hsseo0501/databasemanager/DatabaseManagerApplication.java
@@ -2,7 +2,9 @@ package io.github.hsseo0501.databasemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class DatabaseManagerApplication {
 
