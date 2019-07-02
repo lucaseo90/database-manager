160c3886b1f515101a7b6c0c61afd9e3fc6e71c5
Hyunseok Seo <hsseo0501@gmail.com>
2019-07-03 00:15:45 +0900

    [BACK-SPRING-C1] Added CrossOrigin

--- src/main/java/io/github/hsseo0501/databasemanager/controller/DatabaseController.java
+++ src/main/java/io/github/hsseo0501/databasemanager/controller/DatabaseController.java
@@ -8,15 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RequestMapping(value = "/database", produces = MediaType.APPLICATION_JSON_VALUE)
 public class DatabaseController {
     private static final Logger logger = LoggerFactory.getLogger(DatabaseController.class);
