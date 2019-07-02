1d801beffc499a06b5f49de45e7e99eecfb6d8d2
Hyunseok Seo <hsseo0501@gmail.com>
2019-07-02 16:49:42 +0900

    [FRONT-REACT-C2] Added DatabaseManagerApp

---
+++ frontend/src/component/DatabaseManagerApp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DatabaseManagerApp extends Component {
+    render() {
+        return (
+              <h1>Database Manager Application</h1>
+        )
+    }
+}
+
+export default DatabaseManagerApp
