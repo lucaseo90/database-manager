e933c0674e8a58f1e751a2d1bfc3a37bca8fd0c7
Hyunseok Seo <hsseo0501@gmail.com>
2019-07-05 13:56:31 +0900

    [FRONT-REACT-C11] Added Table, need to imporve   - https://plnkr.co/edit/ysN4Qb3lUp9mbqFhumWJ?p=preview

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,39 @@ import './App.css';
 import DatabaseManagerApp from './component/DatabaseManagerApp';
 import ListQueryResultsComponent from "./component/ListQueryResultsComponent";
 import InputParentForm from "./component/InputParentForm";
+import Table from "./component/Table";
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            tableData:[
+                {'fruit': 'Apple', 'cost': 100},
+                {'fruit': 'Orange', 'cost': 50},
+                {'fruit': 'Banana', 'cost': 35},
+                {'fruit': 'Mango', 'cost': 70},
+                {'fruit': 'Pineapple', 'cost': 45},
+                {'fruit': 'Papaya', 'cost': 40},
+                {'fruit': 'Watermelon', 'cost': 35}
+          ],
+          tableData2:[
+                {'Name': 'Abc', 'Age': 15, 'Location': 'Bangalore'},
+                {'Name': 'Def', 'Age': 43, 'Location': 'Mumbai'},
+                {'Name': 'Uff', 'Age': 30, 'Location': 'Chennai'},
+                {'Name': 'Ammse', 'Age': 87, 'Location': 'Delhi'},
+                {'Name': 'Yysse', 'Age': 28, 'Location': 'Hyderabad'}
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="container">
                 <DatabaseManagerApp />
                 <InputParentForm store={this.props.store}/>
+                <br/> Table 1 data
+                <Table data={this.state.tableData}/>
                 <ListQueryResultsComponent />
             </div>
         );
